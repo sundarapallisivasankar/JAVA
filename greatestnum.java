e1dd97b1f24298c4d1750eee17ab0387610b6a53
sundarapallisivasankar <166827964+sundarapallisivasankar@users.noreply.github.com>
2024-07-10 12:08:55 +0530

    Add files via upload

---
+++ greatestnum.java
@@ -0,0 +1,11 @@
+public class greatestnum{
+	public static void main(String[] args){
+		int a=10,b=60,c=30;
+		if(a>b && a>c)		
+			System.out.println("a is greater");
+		else if(b>a && b>c)
+			System.out.println("b is greater");
+		else
+			System.out.println("c is greater");
+	}
+}
